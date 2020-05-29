750ee576e09bc47aab187c824b41febeda5667c3
Leandro <lepehan@gmail.com>
2020-05-29 18:34:38 -0400

    added initial files + heroku config

--- bot/credentials.py
+++ bot/credentials.py
@@ -1,3 +1,3 @@
-bot_token = '1051506919:AAHWKrM1t4lsEWZ09HBPSUsEzD3hAGck_WQ
+bot_token = '1051506919:AAHWKrM1t4lsEWZ09HBPSUsEzD3hAGck_WQ'
 bot_username = 'le_helper_bot'
 URL = 'https://telegram-helper-lh.herokuapp.com/'
