47a548ee638dc150aa454dab22fd360a510fec05
Leandro <lepehan@gmail.com>
2020-05-29 18:17:25 -0400

    added initial files + heroku config

---
+++ bot/bot.py
@@ -0,0 +1,7 @@
+def get_response(msg):
+    """
+    you can place your mastermind AI here
+    could be a very basic simple response like "معلش"
+    or a complex LSTM network that generate appropriate answer
+    """
+    return "معلش !"
