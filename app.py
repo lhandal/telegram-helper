5cf7488a2376564d3a07374bd580e0cc0aa3a532
Leandro <lepehan@gmail.com>
2020-05-29 20:38:46 -0400

    added /start

--- app.py
+++ app.py
@@ -11,16 +11,15 @@ bot = telegram.Bot(token=TOKEN)
 
 app = Flask(__name__)
 
-@app.route('/{}'.format(TOKEN), methods=['POST'])
 updater = Updater(token='TOKEN', use_context=True)
 dispatcher = updater.dispatcher
-
-def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
-
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 updater.start_polling()
+
+@app.route('/{}'.format(TOKEN), methods=['POST'])
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
 
 @app.route('/setwebhook', methods=['GET', 'POST'])
