004731cd1643e62f96c8e9c1e958a87a9a861c62
Leandro <lepehan@gmail.com>
2020-05-30 00:02:35 -0400

    added /start

--- app.py
+++ app.py
@@ -27,57 +27,26 @@ app = Flask(__name__)
 
 @app.route('/{}'.format(TOKEN), methods=['POST'])
 def helper():
-    from telegram.ext import Updater, InlineQueryHandler, CommandHandler
-    import requests
-    import re
-    from credentials import bot_token
 
-    def get_url():
-        contents = requests.get('https://random.dog/woof.json').json()
-        url = contents['url']
-        return url
+    # Retrieve the message in JSON and then transform it to Telegram object
+    update = telegram.Update.de_json(request.get_json(force=True), bot)
+    print(update.message)
+    incoming_message, msg_id, chat_id, name, lastname = parse_message(update)
 
-    def get_image_url():
-        allowed_extension = ['jpg', 'jpeg', 'png']
-        file_extension = ''
-        while file_extension not in allowed_extension:
-            url = get_url()
-            file_extension = re.search("([^.]*)$", url).group(1).lower()
-        return url
+    print("Got text message:", incoming_message)
+    get_response(incoming_message, chat_id, msg_id, name, lastname)
+    return 'ok'
 
-    def bop(bot, update):
-        url = get_image_url()
-        chat_id = update.message.chat_id
-        bot.send_photo(chat_id=chat_id, photo=url)
-
-    def main():
-        updater = Updater(TOKEN)
-        dp = updater.dispatcher
-        dp.add_handler(CommandHandler('bop', bop))
-        updater.start_polling()
-        updater.idle()
-
-    main()
-
-    # # Retrieve the message in JSON and then transform it to Telegram object
-    # update = telegram.Update.de_json(request.get_json(force=True), bot)
-    # print(update.message)
-    # incoming_message, msg_id, chat_id, name, lastname = parse_message(update)
-    #
-    # print("Got text message:", incoming_message)
-    # get_response(incoming_message, chat_id, msg_id, name, lastname)
-    # return 'ok'
-
-# @app.route('/setwebhook', methods=['GET', 'POST'])
-# def set_webhook():
-#     # we use the bot object to link the bot to our app which live
-#     # in the link provided by URL
-#     s = bot.setWebhook('{URL}{HOOK}'.format(URL=URL, HOOK=TOKEN))
-#     # something to let us know things work
-#     if s:
-#         return "Webhook setup ok!"
-#     else:
-#         return "Webhook setup failed..."
+@app.route('/setwebhook', methods=['GET', 'POST'])
+def set_webhook():
+    # we use the bot object to link the bot to our app which live
+    # in the link provided by URL
+    s = bot.setWebhook('{URL}{HOOK}'.format(URL=URL, HOOK=TOKEN))
+    # something to let us know things work
+    if s:
+        return "Webhook setup ok!"
+    else:
+        return "Webhook setup failed..."
 
 @app.route('/')
 def index():
@@ -87,4 +56,3 @@ if __name__ == '__main__':
     # note the threaded arg which allow
     # your app to have more than one thread
     app.run(threaded=True)
-    main()
