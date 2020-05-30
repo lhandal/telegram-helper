0d732a4882b68c0ff5fccb2190025bcb928fc180
Leandro <lepehan@gmail.com>
2020-05-29 22:59:03 -0400

    added /start

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ app = Flask(__name__)
 def helper():
     print(telegram.Update)
     print(request.get_json(force=True))
-    print(telegram.User.full_name())
+    print(telegram.User.de_json(request.get_json(force=True), bot))
     # Retrieve the message in JSON and then transform it to Telegram object
     update = telegram.Update.de_json(request.get_json(force=True), bot)
     print(update)
