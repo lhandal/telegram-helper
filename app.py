a252321c06f180f85334991ed14b5bab68b1b6fa
Leandro <lepehan@gmail.com>
2020-05-29 21:59:19 -0400

    added /start

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ updater = Updater(TOKEN, use_context=True)
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
 updater.start_polling()
 updater.idle()
-
+    #x
     # print("Got text message:", incoming_message)
     # get_response(incoming_message, chat_id, msg_id, name, lastname)
     # return 'ok'
